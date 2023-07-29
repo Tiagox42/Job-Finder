04f5561e513f90c170be1eba57dd52b2d6faadb1
Tiago Dantas <tiagodantas42@gmail.com>
2023-07-29 19:58:28 -0300

    main e add em rotas funcionando

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 // handle bars
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // db connection
 db
@@ -31,7 +34,7 @@ db
 
 // routes
 app.get('/', (req, res) => {
-    res.send("Está funcionando");
+    res.render('index');
 });
 
 // jobs routes
