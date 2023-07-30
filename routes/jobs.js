5c01343e91dca4249917a1fa56eb8da8e54acaa6
Tiago Dantas <tiagodantas42@gmail.com>
2023-07-30 12:00:32 -0300

    finally
    add responsive layout

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,10 +2,21 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
 
+// rota de teste
 router.get('/test', (req, res) => {
     res.send('deu certo');
 });
 
+//detalhe da vaga
+router.get('/view/:id', (req, res) => Job.findOne({
+    where: {id: req.params.id}
+}).then(job => {
+    res.render('view', {
+        job
+    });
+}).catch(err => console.log(err)));
+
+// form da rota de envio
 router.get('/add', (req, res) => {
     res.render('add');
 })
@@ -21,6 +32,7 @@ router.post('/add', (req, res) => {
         description,
         salary,
         company,
+        email,
         new_job
     })
     .then(() => res.redirect('/'))
